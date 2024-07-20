52d240c97c2805c04dda9626e328aadb5434ff59
NoeZM <noe.zm2000@gmail.com>
2024-07-19 23:53:54 -0600

    cambio de tarifa

--- Caseta/Cobro/cobroFONLow.py
+++ Caseta/Cobro/cobroFONLow.py
@@ -653,73 +653,32 @@ class FormularioOperacion:
         importe = 0
         cuarto_hora = 0
 
-        # Calcula la tarifa y el importe a pagar
+        # Determina el cuarto de hora
         if self.minutos_dentro == 0:
             cuarto_hora = 0
-        elif self.minutos_dentro < 16 and self.minutos_dentro >= 1:
+        elif 1 <= self.minutos_dentro < 16:
             cuarto_hora = 1
-        elif self.minutos_dentro < 31 and self.minutos_dentro >= 16:
+        elif 16 <= self.minutos_dentro < 31:
             cuarto_hora = 2
-        elif self.minutos_dentro < 46 and self.minutos_dentro >= 31:
+        elif 31 <= self.minutos_dentro < 46:
             cuarto_hora = 3
-        elif self.minutos_dentro <= 59 and self.minutos_dentro >= 46:
+        elif 46 <= self.minutos_dentro <= 59:
             cuarto_hora = 4
 
-        if self.horas_dentro <= 2:
-            if self.horas_dentro == 2 and cuarto_hora == 0:
-                importe = 5
-            elif self.horas_dentro == 2 and cuarto_hora >= 0:
-                importe = 10
-            else:
-                importe = 5
-
-        # Calcula el importe a pagar según la tabla de precios
-        elif 2 <= self.horas_dentro <= 4:
-            if self.horas_dentro == 4 and cuarto_hora == 0:
-                importe = 10
-            elif self.horas_dentro == 4 and cuarto_hora >= 0:
-                importe = 15
-            else:
-                importe = 10
-
-        elif 4 < self.horas_dentro <= 6:
-            if self.horas_dentro == 6 and cuarto_hora == 0:
-                importe = 15
-            elif self.horas_dentro == 6 and cuarto_hora >= 0:
-                importe = 20
-            else:
-                importe = 15
-
-        elif 6 < self.horas_dentro <= 8:
-            if self.horas_dentro == 8 and cuarto_hora == 0:
-                importe = 20
-            elif self.horas_dentro == 8 and cuarto_hora >= 0:
-                importe = 25
-            else:
-                importe = 20
-
-        elif 8 < self.horas_dentro <= 10:
-            if self.horas_dentro == 10 and cuarto_hora == 0:
-                importe = 25
-            elif self.horas_dentro == 10 and cuarto_hora >= 0:
-                importe = 30
-            else:
-                importe = 25
-
-        elif 10 < self.horas_dentro <= 12:
-            if self.horas_dentro == 12 and cuarto_hora == 0:
-                importe = 30
-            elif self.horas_dentro == 12 and cuarto_hora >= 0:
-                importe = 500
-            else:
-                importe = 30
-
+        # Determina el importe base según las horas y cuartos de hora
+        if self.horas_dentro < 2 or (self.horas_dentro == 2 and cuarto_hora == 0):
+            importe = 5
         else:
-            if self.horas_dentro == 12 and cuarto_hora == 0:
-                importe = 30
-            else:
-                importe = 500
+            horas_completas = self.horas_dentro
+            if cuarto_hora > 0:
+                horas_completas += 1
+            
+            if horas_completas % 2 == 0:
+                horas_completas -= 1
 
+            importe = 5 + ((horas_completas - 1) // 2) * 5
+            
+        importe = importe + (60 * self.dias_dentro)
 
         # Establecer el importe y mostrarlo
         self.mostrar_importe(importe)
--- test.py
+++ test.py
@@ -1,78 +1,92 @@
 class a:
-    def __init__(self):
-        
-        self.minutos_dentro = 23
-        self.horas_dentro = 5
+    def __init__(self, horas_dentro, minutos_dentro):
+        self.horas_dentro = horas_dentro
+        self.minutos_dentro = minutos_dentro
 
-        importe = 0
-
-        # Calcula la tarifa y el importe a pagar
+    def calcular_importe(self):
+        # Determina el cuarto de hora
         if self.minutos_dentro == 0:
             cuarto_hora = 0
-        elif self.minutos_dentro < 16 and self.minutos_dentro >= 1:
+        elif 1 <= self.minutos_dentro < 16:
             cuarto_hora = 1
-        elif self.minutos_dentro < 31 and self.minutos_dentro >= 16:
+        elif 16 <= self.minutos_dentro < 31:
             cuarto_hora = 2
-        elif self.minutos_dentro < 46 and self.minutos_dentro >= 31:
+        elif 31 <= self.minutos_dentro < 46:
             cuarto_hora = 3
-        elif self.minutos_dentro <= 59 and self.minutos_dentro >= 46:
+        elif 46 <= self.minutos_dentro <= 59:
             cuarto_hora = 4
 
-        if self.horas_dentro <= 2:
-            if self.horas_dentro == 2 and cuarto_hora == 0:
-                importe = 5
-            elif self.horas_dentro == 2 and cuarto_hora >= 0:
-                importe = 10
-            else:
-                importe = 5
-
-        # Calcula el importe a pagar según la tabla de precios
-        elif 2 <= self.horas_dentro <= 4:
-            if self.horas_dentro == 4 and cuarto_hora == 0:
-                importe = 10
-            elif self.horas_dentro == 4 and cuarto_hora >= 0:
-                importe = 15
-            else:
-                importe = 10
-
-        elif 4 < self.horas_dentro <= 6:
-            if self.horas_dentro == 6 and cuarto_hora == 0:
-                importe = 15
-            elif self.horas_dentro == 6 and cuarto_hora >= 0:
-                importe = 20
-            else:
-                importe = 15
-
-        elif 6 < self.horas_dentro <= 8:
-            if self.horas_dentro == 8 and cuarto_hora == 0:
-                importe = 20
-            elif self.horas_dentro == 8 and cuarto_hora >= 0:
-                importe = 25
-            else:
-                importe = 20
-
-        elif 8 < self.horas_dentro <= 10:
-            if self.horas_dentro == 10 and cuarto_hora == 0:
-                importe = 25
-            elif self.horas_dentro == 10 and cuarto_hora >= 0:
-                importe = 30
-            else:
-                importe = 25
-
-        elif 10 < self.horas_dentro <= 12:
-            if self.horas_dentro == 12 and cuarto_hora == 0:
-                importe = 30
-            elif self.horas_dentro == 12 and cuarto_hora >= 0:
-                importe = 500
-            else:
-                importe = 30
-
+        # Determina el importe base según las horas y cuartos de hora
+        if self.horas_dentro < 2 or (self.horas_dentro == 2 and cuarto_hora == 0):
+            importe = 5
         else:
-            if self.horas_dentro == 12 and cuarto_hora == 0:
-                importe = 30
-            else:
-                importe = 500
+            horas_completas = self.horas_dentro
+            if cuarto_hora > 0:
+                horas_completas += 1
+            
+            if horas_completas % 2 == 0:
+                horas_completas -= 1
 
-        print(importe)
+            importe = 5 + ((horas_completas - 1) // 2) * 5
+        
+        return importe
 
-a()
+file = open('TestTarifa.py', "w", encoding="UTF-8")
+code = """import unittest
+from test import a
+
+class TestCobroController(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)\n"""
+file.write(code)
+
+for hour in range(25):  # Incluye 24 horas completas
+    for minutos_dentro in range(0, 60):
+        importe = 0
+
+        if minutos_dentro == 0:
+            cuarto_hora = 0
+        elif 1 <= minutos_dentro < 16:
+            cuarto_hora = 1
+        elif 16 <= minutos_dentro < 31:
+            cuarto_hora = 2
+        elif 31 <= minutos_dentro < 46:
+            cuarto_hora = 3
+        elif 46 <= minutos_dentro <= 59:
+            cuarto_hora = 4
+
+        # Determina el importe base según las horas y cuartos de hora
+        if hour < 2 or (hour == 2 and cuarto_hora == 0):
+            importe = 5
+        else:
+            horas_completas = hour
+            if cuarto_hora > 0:
+                horas_completas += 1
+            
+            if horas_completas % 2 == 0:
+                horas_completas -= 1
+
+            importe = 5 + ((horas_completas - 1) // 2) * 5
+
+        if hour > 24:
+            importe = 65
+
+        code = f"""
+    def test_{hour}_horas_{minutos_dentro}_minutos(self):
+        instancia = a({hour}, {minutos_dentro})
+        self.assertEqual(instancia.calcular_importe(), {importe})\n"""
+
+        file.write(code)
+
+code = """
+def run_code():
+    unittest.main()\n\n    
+"""
+file.write(code)
+
+code = """
+if __name__ == '__main__':
+    run_code()\n\n"""
+file.write(code)
+
+file.close()
